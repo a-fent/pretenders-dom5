64763eb405d7fb722deffb2874085d5b0c8b7914
Alex Fenton <alex@pressure.to>
2020-02-14 07:58:59 +0100

    Updates

---
+++ precache-manifest.b440c69f1e34d17750eb3a0f80720b24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d3c12c983a737323e4fb1d22978075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f66b8084d8ea7f686f9c",
+    "url": "/static/css/main.61f2f4a3.chunk.css"
+  },
+  {
+    "revision": "97ad017132af60bf6367",
+    "url": "/static/js/2.7ff5723a.chunk.js"
+  },
+  {
+    "revision": "78b86c0095c74f94992db9795d80e258",
+    "url": "/static/js/2.7ff5723a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66b8084d8ea7f686f9c",
+    "url": "/static/js/main.89956692.chunk.js"
+  },
+  {
+    "revision": "3011951142926e2bae10",
+    "url": "/static/js/runtime-main.1ab1f92d.js"
+  }
+]);
